dc148baf1b9549cf2d77780556f836588680d4c0
takusnsd <68777877+takusnsd@users.noreply.github.com>
2020-09-02 21:06:35 +0900

    New_search_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'messages#index'
   root "groups#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
